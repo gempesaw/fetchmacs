9caeccb4dde74789a54478f8a7ce3a772f582c5b
Daniel Gempesaw <gempesaw@gmail.com>
2012-10-06 01:11:40 -0400

    Cleanup unused functions, add some documentation

--- auth.el
+++ auth.el
@@ -2,13 +2,10 @@
 (require 'json)
 (load-file "api-urls.el")
 
-http://www.fetchnotes.com/keys?
+(defun fetchmacs-url-http-post (url args)
+  "Send ARGS to URL as a POST request.
 
-(setq my-url "http://www.fetchnotes.com/keys")
-(setq my-args '(("name" . "fetchmacs" ) ("email" . "gempesaw@gmail.com") ("username" . "gempesaw@gmail.com") ("password" . "DgqyT16sC90nd9JA")))
-
-(defun my-url-http-post (url args)
-  "Send ARGS to URL as a POST request."
+Returns JSON body."
   (let ((url-request-method "POST")
         (url-request-extra-headers
          '(("Content-Type" . "application/x-www-form-urlencoded")))
@@ -18,20 +15,7 @@ http://www.fetchnotes.com/keys?
                               "="
                               (url-hexify-string (cdr arg))))
                     args
-                    "&")))
-    ;; if you want, replace `my-switch-to-url-buffer' with `my-kill-url-buffer'
-    (url-retrieve url 'my-switch-to-url-buffer)))
-
-(defun my-kill-url-buffer (status)
-  "Kill the buffer returned by `url-retrieve'."
-  (kill-buffer (current-buffer)))
-
-(defun my-switch-to-url-buffer (status)
-  "Switch to the buffer returned by `url-retreive'.
-    The buffer contains the raw HTTP response sent by the server."
-  (switch-to-buffer (current-buffer)))
 
 
-(my-url-http-post my-url my-args)
 
-()
+
