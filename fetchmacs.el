345ae013f3db91074ebd303be6d0c377415ed9fb
Daniel Gempesaw <gempesaw@gmail.com>
2012-10-08 23:49:29 -0400

    Flesh out fetchmacs mode map and mode hook

--- fetchmacs.el
+++ fetchmacs.el
@@ -12,8 +12,7 @@
     (when (string= (cdr (assoc 'status json-response-as-alist)) 'success))
     (setq fetchmacs-all-notes (cdr (assoc 'response json-response-as-alist)))))
 
-
-
+(defvar fetchmacs-mode-hook nil)
 (put 'fetchmacs-mode 'mode-class 'special)
 (defun fetchmacs-mode ()
 "View all of your fetchnotes and act on them.
@@ -28,3 +27,27 @@
         mode-line-process "")
   (use-local-map fetchmacs-mode-map)
   (run-mode-hooks 'fetchmacs-mode-hook))
+
+(defvar fetchmacs-mode-map
+  (let ((map (make-keymap)))
+    (suppress-keymap map)
+    (define-key map (kbd "n") 'fetchmacs-goto-next-note)
+    (define-key map (kbd "p") 'fetchmacs-goto-previous-note)
+    (define-key map (kbd "g") 'fetchmacs-refresh)
+
+    (define-key map (kbd "c") 'fetchmacs-create-new-note)
+
+    (define-key map (kbd "e") 'fetchmacs-edit-note-at-point)
+    (define-key map (kbd "o") 'fetchmacs-edit-note-at-point)
+    (define-key map (kbd "RET") 'fetchmacs-edit-note-at-point)
+
+    (define-key map (kbd "/") 'fetchmacs-search)
+    (define-key map (kbd "t") 'fetchmacs-filter-by-tag)
+
+    (define-key map (kbd "d") 'fetchmacs-delete-note-at-point)
+
+    (define-key map (kbd "?") 'describe-mode)
+    (define-key map (kbd "h") 'describe-mode)
+    (define-key map (kbd "q") 'quit-window)
+    (define-key map (kbd "z") 'kill-this-buffer)
+    ))
