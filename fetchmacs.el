7a591f89bb5699ad3aede40b52a47b2a34b87024
Daniel Gempesaw <gempesaw@gmail.com>
2013-03-23 13:26:05 -0400

    Add comments for marmalade packaging

--- fetchmacs.el
+++ fetchmacs.el
@@ -1,3 +1,24 @@
+;;; fetchmacs.el --- A major mode for the Fetchnotes note taking service
+
+;; Copyright 2012-2013 Daniel Gempesaw
+
+;; Author: Daniel Gempesaw <gempesaw@gmail.com>
+;; URL: http://github.com/gempesaw/fetchmacs
+;; Version: 1.0.0
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (require 'url)
 (require 'json)
 (require 'erc-button)
@@ -329,3 +350,6 @@ fetchnotes"
 (defun fetchmacs-search ()
   (interactive)
     (fetchmacs-view-notes (read-string "What do you want to search for? ")))
+(provide 'fetchmacs)
+
+;;; fetchmacs-mode.el ends here
