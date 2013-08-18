2bf5ef6857ce129cb480ba8fc6b96819750af4b5
gempesaw <gempesaw@gmail.com>
2013-08-18 16:01:55 -0400

    Write one failing test - provision keys from deprecated API

--- fetchmacs-ert.el
+++ fetchmacs-ert.el
@@ -1,16 +1,22 @@
-(global-set-key (kbd "<f5>")
+(global-set-key (kbd "C-c <f5>")
                 (lambda ()
                   (interactive)
                   (compile
                    (concat
-                    "emacs -batch  -f ert-run-tests-batch-and-exit "
-                    ;; "-L \"" default-directory "\ "
-                    "-L ert "
-                    "-L fetchmacs-ert.el "))))
+                    "emacs -batch "
+                    "-L \"" default-directory "\" "
+                    "-l ert "
+                    "-l \"fetchmacs-creds.el\" "
+                    "-l \"fetchmacs-ert.el\" "
+                    "-f ert-run-tests-batch-and-exit "))))
 
 (require 'fetchmacs)
+(require 'fetchmacs-creds)
 
 (ert-deftest should-provision-key-pair ()
-  (let ((user fetchmacs-dev-email)
-        (pass (insert-file-contents ".creds")))
-    (should (message "%s:%s" user pass))))
+  (let ((user fetchmacs-my-email)
+        (pass fetchmacs-my-pass))
+    (fetchmacs-provision-keys-for-user user pass)
+    (should-not (eql nil fetchmacs-public-key))
+    (should-not (eql nil fetchmacs-private-key))
+    (should-not (eql nil fetchmacs-author))))
